69c4e88b658ea48c658a01c207ad6b9e0ccf5095
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-25 18:28:07 +0800

    Update index.js

--- src/http/any-catchall/index.js
+++ src/http/any-catchall/index.js
@@ -29,7 +29,24 @@ async function handler (req) {
     proxyhost=proxyhostarr.join('/')
   console.log(proxyhost)
   console.log('))'+proxyurl)
-  const response = await fetch(proxyurl,{})
+  const response = await fetch(proxyurl,{ headers: {
+    'authority': 'askjavascript.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'cache-control': 'max-age=0',
+    'dnt': '1',
+    'if-modified-since': 'Wed, 24 Aug 2022 11:07:36 GMT',
+    'referer': 'https://cn.bing.com/',
+    'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Microsoft Edge";v="104"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.63'
+}})
   text=await response.text()
   rheads=Object(response.headers.raw())
   console.log()
